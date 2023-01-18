a9c6e07100d032be05577e12ed5ed5df574a4d9f
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-01-18 10:03:03 +0000

    Updates to allow for fly deployment

--- server/twitter_django/settings.py
+++ server/twitter_django/settings.py
@@ -28,15 +28,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default="YOUR VERY SECREY KEY")
 # DEBUG = False
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://falling-tree-8142.fly.dev"]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", ]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000",
+                        "https://falling-tree-8142.fly.dev"]
 
-
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
-    # CORS_ALLOWED_ORIGINS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
